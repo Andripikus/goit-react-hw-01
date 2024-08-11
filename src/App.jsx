4cd1cf14aefc5e39b9c718b0b17daad64cfe297b
Andriy Muzyka <andripikus@gmail.com>
2024-08-11 17:05:20 +0200

    hw-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Profile from "./components/Profile/Profile";
+import userData from "./data/profile.json";
+import FriendList from "./components/FriendList/FriendList";
+import friends from "./data/friendList.json";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import transactions from "./data/transactionHistory.json";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
+    <div className="container">
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <>
+        <FriendList friends={friends} />
+      </>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <TransactionHistory items={transactions} />
       </div>
-      <h1>Vite + React is cool</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
